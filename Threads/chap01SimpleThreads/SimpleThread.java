09d4b4bc86d19e9426b906633c035b13eaff0e1c
jaba1163 <rekahjaba@gmail.com>
2022-05-22 18:37:07 -0400

    thread using runnable interface

---
+++ Threads/chap01SimpleThreads/SimpleThread.java
@@ -0,0 +1,22 @@
+package Threads.chap01SimpleThreads;
+
+public class SimpleThread implements Runnable
+{
+
+    @Override
+    public void run() {
+        for (int i=0;i<10;i++)
+        {
+            System.out.println("I love threads. := " + i);
+        }
+    }
+
+public static void main(String args[])
+{
+    System.out.println("In main thread - begin");
+    SimpleThread simpleThread =  new SimpleThread();
+    simpleThread.run();
+    System.out.println("In main thread - end");
+}
+    
+}
